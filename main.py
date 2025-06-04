5e71abb349dc9b98f69e8bb05f6174074b23d24a
j0j0187 <jonte.jarecki@icloud.com>
2025-06-04 12:34:55 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+
+while True:
+ var1 = input("...,")
+ if var1 is fckyou:
+  exit()
+ if var1.endswith("LS"):
+  zvar1 = int(input("...,"))
+  rvar = int(input("...,"))
+  zvar2 = int(input("...,"))
+  if rvar == +:
+   summep  zvar1 + zvar
+   print(summep)
+   break
+  if rvar == -:
+   summem = zvar1 - zvar2
+   print(summep)
+   break
+  if rvar == /:
+   summege = zvar1 / zvar2
+   print(summege)
+   break
+  if rvar == *:
+   summema = zvar1 * zvar2
+   print(summema)
+   break
+  
