13f800f8899cccf05a690a5fbedb74a05b560f62
j0j0187 <jonte.jarecki@icloud.com>
2025-06-05 07:30:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+import os
+
 while True:
  var1 = input("...,")
  if var1.endswith("fckyou"):
@@ -21,3 +24,27 @@ while True:
   if rvar == "*":
    summema = zvar1 * zvar2
    print(summema)
+ if var1 == ("help"):
+  print("You want more tipps, press Enter")
+  input("Press Enter, NOW!!!!!!...,")
+  Iamworking = random.randint(1, 3)
+  if Iamworking == 1:
+   print("For help write help, oh you do that alraedy than fckyou")
+  if  Iamworking == 2:
+   print("For help write help, oh you do that alraedy than fckyou")
+  if Iamworking == 3: 
+    print("Sorry I am working. When you dont understood this it's says that i am about to Fuck you.")
+ if var1 == "mkson":
+  answer = input("You are to dumb for this Command, but you can level up. What is 3859+394759...,")
+  if answer == "idk":
+   sonname = input("Name of your Son:")
+   try:
+    os.makedirs(sonname)
+    print(f"Son '{sonname}' wurde vieleicht erstellt")
+   except FileExistsError:
+    print(f"Ordner '{sonname}' existiert bereits.")
+  else:
+   print("You are to dumb fckyou")
+   input("...,")
+   print("Like Realy dumb.")
+   break
