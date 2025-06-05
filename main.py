0a7227fae9935efa18a1bc09ecb0b37b6e79d2d7
j0j0187 <jonte.jarecki@icloud.com>
2025-06-05 14:35:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import random
 import os
 
 while True:
- var1 = input("...,")
+ var1 = input(",...")
+
  if var1.endswith("fckyou"):
   break
+
+ if var1.endswith("ls"):
+  print("OH sorry but this isn't Bash, try 'LS'")
+
  if var1.endswith("LS"):
-  zvar1 = int(input("...,"))
-  rvar = input("...,")
-  zvar2 = int(input("...,"))
+  print("Oh Maths")
+  zvar1 = int(input(",..."))
+  rvar = input(",...")
+  zvar2 = int(input(",..."))
+
   if rvar == "buff":
    summep = zvar1 + zvar2
    print(summep)
@@ -24,18 +31,24 @@ while True:
   if rvar == "*":
    summema = zvar1 * zvar2
    print(summema)
+
  if var1 == ("help"):
   print("You want more tipps, press Enter")
   input("Press Enter, NOW!!!!!!...,")
   Iamworking = random.randint(1, 3)
+
   if Iamworking == 1:
    print("For help write help, oh you do that alraedy than fckyou")
+
   if  Iamworking == 2:
    print("For help write help, oh you do that alraedy than fckyou")
+
   if Iamworking == 3: 
     print("Sorry I am working. When you dont understood this it's says that i am about to Fuck you.")
+
  if var1 == "mkson":
   answer = input("You are to dumb for this Command, but you can level up. What is 3859+394759...,")
+
   if answer == "idk":
    sonname = input("Name of your Son:")
    try:
@@ -45,6 +58,11 @@ while True:
     print(f"Ordner '{sonname}' existiert bereits.")
   else:
    print("You are to dumb fckyou")
-   input("...,")
+   input(",...")
    print("Like Realy dumb.")
    break
+ if var1 == "diehabenmichvonmeinem12jährigensohnentfernt":
+  papahierbinich = os.getcwd()
+  print(papahierbinich)
+ if var1 == "cson":
+  print("If you wanna change the Son youse change the position of the Folder you in your File Explorer.")
