f5d5b43442b3757eb48129b743a146cadecb65a4
j0j0187 <jonte.jarecki@icloud.com>
2025-06-09 17:04:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import random
 import os
+import webbrowser
+
+
 
 while True:
  var1 = input(",...")
 
+ if var1 == "LinuxSucks":
+  print("Okay, when Linux Sucks why Microsoft use Linux.")
+
  if var1.endswith("fckyou"):
   break
+
+ if var1 == "btwiusearch":
+  print("Great and I use arch BTW")
+  webbrowser.open("https://wiki.archlinux.org/title/Main_page")
 
  if var1.endswith("ls"):
   print("OH sorry but this isn't Bash, try 'LS'")
@@ -28,9 +38,18 @@ while True:
    summege = zvar1 / zvar2
    print(summege)
    
-  if rvar == "*":
+  if rvar == "Studsx2":
    summema = zvar1 * zvar2
    print(summema)
+
+ if var1 == "H":
+  print("Wrong command! Write 'Help' for help.")
+
+ if var1 == "Help":
+  print("Wrong command ...you!!Write 'h' for help.")
+
+ if var1 == "h":
+  print("Wrong command ...you!!!Write 'help' for help.")
 
  if var1 == ("help"):
   print("You want more tipps, press Enter")
@@ -38,10 +57,10 @@ while True:
   Iamworking = random.randint(1, 3)
 
   if Iamworking == 1:
-   print("For help write help, oh you do that alraedy than fckyou")
+   print("For help write help, oh you do that already than fckyou")
 
   if  Iamworking == 2:
-   print("For help write help, oh you do that alraedy than fckyou")
+   print("For help write help, oh you do that already than fckyou")
 
   if Iamworking == 3: 
     print("Sorry I am working. When you dont understood this it's says that i am about to Fuck you.")
@@ -49,20 +68,29 @@ while True:
  if var1 == "mkson":
   answer = input("You are to dumb for this Command, but you can level up. What is 3859+394759...,")
 
-  if answer == "idk":
+  if answer == "42":
    sonname = input("Name of your Son:")
    try:
     os.makedirs(sonname)
-    print(f"Son '{sonname}' wurde vieleicht erstellt")
+    print(f"Son '{sonname}' maybe successful")
    except FileExistsError:
-    print(f"Ordner '{sonname}' existiert bereits.")
+    print(f"Son exist already'{sonname}' I said you are dumb")
   else:
    print("You are to dumb fckyou")
    input(",...")
    print("Like Realy dumb.")
    break
- if var1 == "diehabenmichvonmeinem12jährigensohnentfernt":
+
+ if var1 == "diehabenmichvonmeinem12jÃ¤hrigensohnentfernt":
   papahierbinich = os.getcwd()
   print(papahierbinich)
+
  if var1 == "cson":
   print("If you wanna change the Son youse change the position of the Folder you in your File Explorer.")
+
+ if var1 == "mkgson":
+  gson = input("Name of your Grandson:")
+  gson2 = input("Second Name of your Grandson")
+  print("You have a grandson oh you are old.")
+  with open(gson, "w") as datei:
+    datei.write(gson2)
